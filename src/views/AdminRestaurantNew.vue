56425dc22e7dfdc855b07db500a4f1384b35431c
bwzhao <bwzhao@mail.fcu.edu.tw>
2022-03-09 23:31:32 +0800

    後台新增餐廳：製作表單

---
+++ src/views/AdminRestaurantNew.vue
@@ -0,0 +1,21 @@
+// ./src/views/AdminRestaurantNew.vue
+<template>
+  <div class="container py-5">
+    <!-- 餐廳表單 AdminRestaurantForm -->
+    <AdminRestaurantForm />
+  </div>
+</template>
+
+<script>
+import AdminRestaurantForm from "../components/AdminRestaurantForm";
+export default {
+  name: "AdminRestaurantNew",
+  components:{
+    AdminRestaurantForm
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
