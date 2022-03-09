663ebec0ece9fd6f18be049acf41f3e79dbd8680
bwzhao <bwzhao@mail.fcu.edu.tw>
2022-03-09 23:17:41 +0800

    feat: 後台刪除餐廳

--- src/components/AdminRestaurantsTable.vue
+++ src/components/AdminRestaurantsTable.vue
@@ -43,6 +43,7 @@
         <button
             type="button"
             class="btn btn-link"
+            @click.stop.prevent="deleteRestaurant(restaurant.id)"
         >
           Delete
         </button>
@@ -335,6 +336,9 @@ export default {
   methods: {
     fetchRestaurants() {
       this.restaurants = dummyData.restaurants
+    },
+    deleteRestaurant(restaurantId){
+      this.restaurants = this.restaurants.filter( restaurant => restaurant.id !== restaurantId)
     }
   }
 }
